a42555d82a9a103d6c5735cbca1c44492104af3a
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-27 22:13:34 +0000

    Automatically backed up by Learn

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -42,7 +42,12 @@ end
 
 describe "pop_with_args" do
   before(:each) do 
-    @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    english_bands_by_city = {
+ :liverpool =>   "The Beatles",
+ :manchester =>  "The Smiths",
+ :coventry =>    "Delia Derbyshire and the BBC Radiophonic Band",
+ :london =>      "Ziggy Stardust and the Spiders from Mars"
+}
     @small_dogs = pop_with_args(@dog_breeds)
   end 
   it "takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them" do 
