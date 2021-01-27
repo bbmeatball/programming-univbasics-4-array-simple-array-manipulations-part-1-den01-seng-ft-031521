6c7524067751fc366438d6f47786fa64f8bbe18d
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-27 22:21:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,7 @@ end
 def pop_with_args (array)
 dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
 shiba_inu=dog_breeds.pop
-p shiba_inu
-Chihuahua=dog_breeds.pop
+#p shiba_inu
+chihuawa_dog=dog_breeds.pop
 p dog_breeds
 end
