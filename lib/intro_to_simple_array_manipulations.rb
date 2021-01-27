9cdbee67aa157b67f9654416961fcafd3902d277
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-27 22:38:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,4 +19,5 @@ def using_shift (array)
  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
  im_so_over_this_city=my_favorite_cities.shift
  p im_so_over_this_city
+ array.shift
 end
